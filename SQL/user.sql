dc658bfcdda998de4c6b0991dccca51089c9f3ad
zdabing <zhaotaibing@gmail>
2018-11-05 01:08:28 +0800

    sql文件

---
+++ SQL/user.sql
@@ -0,0 +1,14 @@
+CREATE TABLE `tb_user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(100) DEFAULT NULL COMMENT '用户名',
+  `password` varchar(100) DEFAULT NULL COMMENT '密码',
+  `name` varchar(100) DEFAULT NULL COMMENT '姓名',
+  `age` int(10) DEFAULT NULL COMMENT '年龄',
+  `sex` tinyint(1) DEFAULT NULL COMMENT '性别:  1男性   2女性',
+  `birthday` timestamp NULL DEFAULT NULL COMMENT '出生日期',
+  `note` varchar(255) DEFAULT NULL COMMENT '备注',
+  `created` timestamp NULL DEFAULT current_timestamp() COMMENT '创建时间',
+  `updated` timestamp NULL DEFAULT current_timestamp() ON UPDATE current_timestamp() COMMENT '更新时间',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`user_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
